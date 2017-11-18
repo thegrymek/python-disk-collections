509088258a8e1350605209356854c57f457c0e6c
thegrymek <grymek4@gmail.com>
2017-11-18 22:45:32 +0100

    New Version!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.rst', 'r') as f:
 
 package_name = 'python-disk-collections'
 url_profile = 'https://github.com/thegrymek'
-version = '0.0.1'
+version = '0.0.2'
 setup(
     name=package_name,
     version=version,
